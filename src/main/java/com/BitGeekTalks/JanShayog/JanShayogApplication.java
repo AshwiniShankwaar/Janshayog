47644585cd11e8657a9cab8fdac5e351acb7b209
Ashwini kumar <academyforbrilliance99@gmail.com>
2023-04-16 23:53:32 +0530

    janshayog request service added

--- src/main/java/com/BitGeekTalks/JanShayog/JanShayogApplication.java
+++ src/main/java/com/BitGeekTalks/JanShayog/JanShayogApplication.java
@@ -2,10 +2,15 @@ package com.BitGeekTalks.JanShayog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class JanShayogApplication {
 
+	@GetMapping("/")
+	public String check(){
+		return "Jan Shayog";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(JanShayogApplication.class, args);
 	}
